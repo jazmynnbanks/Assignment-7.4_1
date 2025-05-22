151ac5d46c6ff5c2b502a5f308c1a07392ca3ac2
jazmy <jazmy@JAZ-LAPTOP>
2025-05-22 17:42:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿var parkingSystem = new ParkingSystem(1, 1, 1);
+
+Console.WriteLine(parkingSystem.AddCar(1));
+Console.WriteLine(parkingSystem.AddCar(2));
+Console.WriteLine(parkingSystem.AddCar(3));
+Console.WriteLine(parkingSystem.AddCar(1));
+
+Console.ReadKey();
+public class ParkingSystem
+{
+    public int SmallSlots { get; set; }
+    public int MediumSlots { get; set; }
+    public int BigSlots { get; set; }
+
+    public ParkingSystem(int smallSlots, int mediumSlots, int bigSlots)
+    {
+        SmallSlots = smallSlots;
+        MediumSlots = mediumSlots;
+        BigSlots = bigSlots;
+    }
+
+    public bool AddCar(int carType)
+    {
+        if (carType == 1 && BigSlots > 0)
+        {
+            BigSlots--;
+            return true;
+        }
+        if (carType == 2 && MediumSlots > 0)
+        {
+            MediumSlots--;
+            return true;
+        }
+        if (carType == 3 && SmallSlots > 0)
+        {
+            SmallSlots--;
+            return true;
+        }
+        return false;
+    }
+
+
+}
+
